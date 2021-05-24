c63e8bf5a13ed987d6e29b130e514439abaafa86
Tyler Kowalski <tkowalski9938@gmail.com>
2021-05-24 18:38:41 -0400

    changed help & added LP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import requests
 from discord.ext.commands import Bot
 #imports the file with the API keys
@@ -13,12 +14,13 @@ def get_ranked_data(ID):
     output = requests.get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + ID + "?api_key=" + config.RIOT_API_KEY)
     return(output.json())
 
+help_command = commands.DefaultHelpCommand(no_category = "Commands")
 #creates an instance of the class "Bot", which will act as the connection to discord, and sets the trigger to "?"
-bot = Bot(command_prefix= "?")
+bot = Bot(command_prefix= "?", help_command = help_command)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity = discord.Activity(name = "FINDING THE BOOSTED SWINE", type = 5))
+    await bot.change_presence(activity = discord.Activity(name = "FINDING THE MOST BOOSTED SWINE", type = 5))
     #prints in terminal
     print("BOOSTED BOT IS ONLINE")
 
@@ -31,7 +33,8 @@ async def ranked_stats(ctx, league_name):
     division = str(ranked_dataJSON[0]["rank"])
     wins = str(ranked_dataJSON[0]["wins"])
     losses = str(ranked_dataJSON[0]["losses"])
-    win_percentage = str(round(float(wins) / (float(wins) + float(losses)), 4))
-    await ctx.channel.send(league_name + " is " + tier + " " + division + "\n" + league_name + " has " + wins + " wins and " + losses + " losses with a " + win_percentage + "% win percentage" + "\nsmells like BOOOOOOOOOOOOOOOOOSTED")
+    LP = str(ranked_dataJSON[0]["leaguePoints"])
+    win_percentage = str(round((float(wins) / (float(wins) + float(losses))* 100), 2))
+    await ctx.channel.send(league_name + " is " + tier + " " + division + " " + LP + "LP" + "\n" + wins + " wins and " + losses + " losses with a " + win_percentage + "% win percentage" + "\nsmells like BOOOOOOOOOOOOOOOOOSTED")
 
 bot.run(config.DISCORD_API_KEY)
