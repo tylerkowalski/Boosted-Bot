f1aa6b1de670c8142e9d575c7ff2781dc7c520ff
Tyler Kowalski <tkowalski9938@gmail.com>
2021-05-24 13:56:47 -0400

    changed bot framework & added requests

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import discord
+import requests
+from discord.ext import commands
 #imports the file with the API keys
 import config
 
+#returns the JSON file with the summonder id # 
+def requestSummonerid(name):
+    return requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + config.RIOT_API_KEY)
+
+
+
+
+
+
+
+
+
+
+
 #creates an instance of the class "Client", which will act as the connection to discord
-client = discord.Client()
+client = commands.Bot(command_prefix= "?")
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity = discord.Activity(name = "FINDING THE BOOSTED SWINE", type = 5))
     #prints in terminal
     print("BOOSTED BOT IS ONLINE")
 
 
-
-
-
-
-
-
-@client.event
-async def on_message(message):
-    #does nothing if message is outputted by the bot
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("?boosted"):
-        await message.channel.send("haha")
 
 client.run(config.DISCORD_API_KEY)
