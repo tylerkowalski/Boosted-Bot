b6a7ebaf439459a7369505ad544f448ff43682d9
Tyler Kowalski <tkowalski9938@gmail.com>
2021-05-24 14:25:21 -0400

    added to the "rank" command

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import discord
 import requests
-from discord.ext import commands
+from discord.ext.commands import Bot
 #imports the file with the API keys
 import config
 
 #returns the JSON file with the summonder id # 
-def requestSummonerid(name):
+def request_id(name):
     return requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + config.RIOT_API_KEY)
 
 
@@ -18,15 +18,21 @@ def requestSummonerid(name):
 
 
 
-#creates an instance of the class "Client", which will act as the connection to discord
-client = commands.Bot(command_prefix= "?")
 
-@client.event
+#creates an instance of the class "Bot", which will act as the connection to discord
+bot = Bot(command_prefix= "?")
+
+@bot.event
 async def on_ready():
-    await client.change_presence(activity = discord.Activity(name = "FINDING THE BOOSTED SWINE", type = 5))
+    await bot.change_presence(activity = discord.Activity(name = "FINDING THE BOOSTED SWINE", type = 5))
     #prints in terminal
     print("BOOSTED BOT IS ONLINE")
 
+#there exists an issue with taking in the username, need to debug
+@bot.command(name = "rank",)
+async def rank(ctx, *league_name):
+    idJSON = request_id(league_name)
+    ID = idJSON[league_name]["id"]
+    print(ID)
 
-
-client.run(config.DISCORD_API_KEY)
+bot.run(config.DISCORD_API_KEY)
